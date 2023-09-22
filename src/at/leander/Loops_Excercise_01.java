c6772679b19f0b4a9e63df52e4ba813b48a069fa
Leander <leander.wachter@student.htldornbirn.at>
2023-09-22 09:07:33 +0200

    Loops_excercise_01 finished

---
+++ src/at/leander/Loops_Excercise_01.java
@@ -0,0 +1,20 @@
+package at.leander;
+import java.util.Random;
+public class Loops_Excercise_01 {
+    public static void main(String[] args) {
+
+//Zähle in einem For-Loop die Zahlen von 1 bis 100 (inklusive) zusammen
+        Random random = new Random();
+    int randomNumber = random.nextInt(100);
+    int count = 0;
+    for (int i = 0; i <= 100; i++) {
+            System.out.println(i+count);
+        }
+
+
+
+
+
+
+    }
+}

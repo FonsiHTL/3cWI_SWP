8d024733188d93567790afd608e806ad888b1656
Leander <leander.wachter@student.htldornbirn.at>
2023-09-22 08:31:10 +0200

    If_Bedingungen_02 finished

--- src/at/leander/If_Bedingungen_02.java
+++ src/at/leander/If_Bedingungen_02.java
@@ -4,20 +4,24 @@ import java.util.Random;
 
 public class If_Bedingungen_02 {
 
-    Random random = new Random();
-    int randomNumber1 = random.nextInt(100);
-    int randomNumber2 = random.nextInt(100);
-        System.out.println(randomNumber1);
-        System.out.println(randomNumber2);
 
-         if ((randomNumber1<randomNumber2) && (randomNumber1>50)){
+    public static void main(String[] args) {
+
+
+    Random random1 = new Random();
+    Random random2 = new Random();
+    int randomNumber1 = random1.nextInt(100);
+    int randomNumber2 = random2.nextInt(100);
+
+
+    if (randomNumber1 < randomNumber2 && randomNumber1 < 50){
         System.out.println("Zahl 1 ist kleiner als Zahl 2 und Mini");
 
-    } else if((randomNumber1<30) || (randomNumber2<30)){
-        System.out.println("Eine der beiden");
+    } else if(randomNumber1<30 || randomNumber2<30){
+        System.out.println("Eine der beiden ist kleiner als 30");
 
-    } else if(randomNumber<50){
-        System.out.println("Large");
+    } else if(randomNumber1<50 && randomNumber2!=50){
+        System.out.println("Erste Zahl klein, zweite kein 50iger");
     }
 
 
@@ -31,5 +35,5 @@ public class If_Bedingungen_02 {
 
 // Wenn die erste Zahl kleiner ist als 50 UND die zweite Zahl ungleich 50 ist
 // dann gib aus "Erste Zahl klein, zweite kein 50iger"
-
+}
 }

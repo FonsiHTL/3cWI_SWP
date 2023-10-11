d938c5490cd355142dac888bf6deae91b7dca78c
Leander <leander.wachter@student.htldornbirn.at>
2023-10-11 08:11:40 +0200

    Caesar_Encryption nearly finished

--- src/at/leander/projects/caesar/Caesar_Encryption.java
+++ src/at/leander/projects/caesar/Caesar_Encryption.java
@@ -1,2 +1,65 @@
-package at.leander.projects.caesar;public class Caesar_Encryption {
+package at.leander.projects.caesar;
+
+public class Caesar_Encryption {
+
+
+    public static void main(String[] args) {
+
+        String normaltext = "Servus!";
+        int key = 2;
+
+        char[] verschluesselterTextArray = encrypt(klartext.toCharArray(), key);
+        String verschluesselterText = new String(verschluesselterTextArray);
+        System.out.println("Verschlüsselter Text: " + verschluesselterText);
+
+        char[] entschluesselterTextArray = decrypt(verschluesselterTextArray, key);
+        String entschluesselterText = new String(entschluesselterTextArray);
+        System.out.println("Entschlüsselter Text: " + entschluesselterText);
+    }
+
+    public static char[] encrypt(char[] textArray, int key) {
+        char[] verschluesselterTextArray = new char[textArray.length];
+
+        for (int i = 0; i < textArray.length; i++) {
+            char zeichen = textArray[i];
+            char verschluesseltesZeichen = zeichen;
+
+            if (Character.isLetter(zeichen)) {
+                int verschiebung = Character.isUpperCase(zeichen) ? 'A' : 'a';
+                verschluesseltesZeichen = (char) (((zeichen - verschiebung + key) % 26) + verschiebung);
+            }
+
+            verschluesselterTextArray[i] = verschluesseltesZeichen;
+        }
+
+        return verschluesselterTextArray;
+    }
+
+    public static char[] decrypt(char[] textArray, int schluessel) {
+        // Die Entschlüsselung ist das Gleiche wie die Verschlüsselung, nur mit negativem Schlüssel
+        return encrypt(textArray, -schluessel);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

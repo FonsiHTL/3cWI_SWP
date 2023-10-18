7d18f7d9d0286eae08e80a26a9ac5076eb0190c2
Leander <leander.wachter@student.htldornbirn.at>
2023-10-18 09:34:23 +0200

    Ratespiel_01 started

---
+++ src/at/leander/projects/Ratespiel_01.java
@@ -0,0 +1,17 @@
+package at.leander.projects;
+
+import java.util.Random;
+
+
+public class Ratespiel_01 {
+
+
+    public static void main(String[] args) {
+        String[] words = {"Simon", "Lukas", "Jonas", "Eric" };
+
+        Random random = new Random();
+
+
+
+    }
+}

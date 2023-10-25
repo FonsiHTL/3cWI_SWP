aafc5ab25940e19339ff4b83471e3c81b41aee63
Leander <leander.wachter@student.htldornbirn.at>
2023-10-25 09:52:30 +0200

    Tic_Tac_Toe_01 started programming field

--- src/at/leander/projects/Tic_Tac_Toe_01.java
+++ src/at/leander/projects/Tic_Tac_Toe_01.java
@@ -1,11 +1,32 @@
 package at.leander.projects;
 
+import javax.naming.PartialResultException;
 import java.util.Scanner;
 
 public class Tic_Tac_Toe_01 {
 
+ public static void main(String[] args) {
+
+     char[][] board = {
+             {' ', ' ', ' '},
+             {' ', ' ', ' '},
+             {' ', ' ', ' '}
+     };
 
 
 
+     for (int row = 0; row < 3; row++) {
+         for (int col = 0; col < 3; col++) {
+             System.out.print(" " + board[row][col] + " ");
+             if (col < 2) {
+                 System.out.print("|");
+             }
+         }
+         System.out.println();
+         if (row < 2) {
+             System.out.println("---+---+---");
+         }
+     }
 
+ }
 }
